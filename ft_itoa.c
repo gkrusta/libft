78ac3aa58d364bbae278e07314654cde79d0fcd4
Gabriela <krustagabriela@gmail.com>
2023-04-28 19:32:53 +0200

    itoa not functioning

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 16:58:11 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/04/27 13:16:43 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/04/28 19:32:40 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,23 +32,25 @@ char	*ft_itoa(int n)
 {
 	char	*result;
 	size_t	len;
+	long	nr;
 
 	len = ft_numblen((long)(n));
+	nr = (long)n;
 	result = malloc(sizeof(char) * (len + 1));
 	if (!result)
 		return (NULL);
 	result[len--] = '\0';
-	if (n < 0)
+	if (nr < 0)
 	{
 		result[0] = '-';
-		n *= (-1);
+		nr *= (-1);
 	}
-	if (n == 0)
+	if (nr == 0)
 		result[0] = '0';
 	while (len > 0)
 	{
-		result[len--] = n % 10 + '0';
-		n /= 10;
+		result[len--] = nr % 10 + '0';
+		nr /= 10;
 	}
 	return (result);
 }

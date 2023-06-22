1faba3e6898bc1edb8101b1e29b7d5b8aaea0b0c
Gabriela <krustagabriela@gmail.com>
2023-06-22 17:03:54 +0200

    Libft restored

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,41 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/29 19:01:07 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/05/01 17:38:47 by gkrusta          ###   ########.fr       */
+/*   Created: 2023/05/01 13:30:16 by gkrusta           #+#    #+#             */
+/*   Updated: 2023/05/01 14:04:34 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_node;
+	t_list	*new_list;
+	t_list	*ptr;
 
-	new_node = (t_list *)malloc(sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
+	new_list = 0;
+	while (lst)
+	{
+		ptr = ft_lstnew(f(lst->content));
+		if (ptr)
+		{
+			ft_lstadd_back(&new_list, ptr);
+			lst = lst->next;
+		}
+		else
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+	}
+	return (new_list);
 }
-
-/* int	main(void)
-{
-	t_list	*head;
-	t_list	*second;
-	t_list	*third;
-
-	head = ft_lstnew(1);
-	second = ft_lstnew(2);
-	third = ft_lstnew(3);
-
-	head->next = second;
-	second->next = third;
-	third->next = NULL;
-	return (0);
-} */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 15:21:13 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/06/22 16:48:00 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/05/01 17:36:36 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,13 +65,12 @@ void	ft_putnbr_fd(int n, int fd);
 /* bonus functions */
 typedef struct s_list
 {
-	int	*content;
-	int	value; // adding these elements for push_swap
-	int	index;
+	void			*content;
 	struct s_list	*next;
 }					t_list;
 
-t_list	*ft_lstnew(int value);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
@@ -79,9 +78,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-
-/* functions added for push_swap */
-long	ft_long_atoi(const char	*str);
-int		ft_strcmp(const char *s1, const char *s2);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
